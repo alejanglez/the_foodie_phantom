3b1edcfc3aaf23cae6d91d390232c72ffb3eb9b5
Rodrigohernandezsan <rodrigoherandezsan@gamil.com>
2020-08-27 12:02:05 +0200

    auth.routes

--- models/User.model.js
+++ models/User.model.js
@@ -10,12 +10,36 @@ const userSchema = new Schema(
       required: [true, 'Username is required.'],
       unique: true
     },
-    
+    email: {
+      type: String,
+     required: [true, 'Email is required.'],
+     unique: true,
+     lowercase: true,
+     trim: true
+    },
     passwordHash: {
       type: String,
       required: [true, 'Password is required.']
-    }
-  },
+    },
+    Date : {
+      type: String,
+      trim: true,
+      required: [true, 'Date of birth is required.'],
+      unique: true
+    },
+    Postcode: {
+      type: String,
+      trim: true,
+      required: [true, 'Postcode is required.'],
+      unique: true
+    },
+    Phone: {
+      type: String,
+      trim: true,
+      required: [true, 'Phone is required.'],
+      unique: true
+      }
+    },
   {
     timestamps: true
   }
