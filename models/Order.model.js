2639e82e76d40d76b0e3f05cae837debdd9f8496
alejanglez <64974849+alejanglez@users.noreply.github.com>
2020-09-03 19:55:44 +0200

    order model

--- models/Order.model.js
+++ models/Order.model.js
@@ -2,19 +2,24 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const chartSchema = new Schema(
-
   {
-    menuOrderedRef: {type: mongoose.ObjectId, ref: "Menu", required: true},
+    menuOrderedRef: { type: mongoose.ObjectId, ref: "Menu", required: true },
     orderOwnerRef: { type: mongoose.ObjectId, ref: "Cook", required: true },
-    orderBuyerRef: {type: mongoose.ObjectId, ref: "User", required: true},
-    quantity: {type: Number, required: true, default: 1, min: [1, "Invalid value for product quantity"], max: [6, "Invalid value for product quantity"] },
-    price: {type: Number, required: true}
+    orderBuyerRef: { type: mongoose.ObjectId, ref: "User", required: true },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+      min: [1, "Invalid value for product quantity"],
+      max: [6, "Invalid value for product quantity"],
+    },
+    price: { type: Number, required: true },
   },
   {
     timestamps: {
       createdAt: "created_at",
-      updatedAt: "updated_at"
-    }
+      updatedAt: "updated_at",
+    },
   }
 );
 
